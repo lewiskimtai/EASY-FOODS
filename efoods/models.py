b082a450b869148190a85daf30c9b13cb2e3aafb
[Lewis Kimtai] <[lewis.kimtai@gmail.com]>
2019-06-13 18:11:37 +0300

    good database relationships created

--- efoods/models.py
+++ efoods/models.py
@@ -5,57 +5,53 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+meals = db.Table('meals',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id')),
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id'))
+)
+
+orders = db.Table('orders',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('meals_id', db.Integer, db.ForeignKey('meals.id'))
+)
+
+class Role(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(20), unique=False, nullable=False)
+    user = db.relationship('User', backref='role')
+    
+    def __repr__(self):
+        return f"User('{self.role}')"
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(20), unique=False, nullable=False)
-    user_email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    role = db.relationship('Role', backref='admin', lazy='dynamic')
-    order = db.relationship('Order', backref='user', lazy='dynamic')
-
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    restaurant = db.relationship('Restaurant', backref='user')
+    food = db.relationship('Food', backref='user')
+    order = db.relationship('meals', secondary=orders, backref=db.backref('customer', lazy='dynamic'))
+    
     def __repr__(self):
-        return f"User('{self.user_name}', '{self.user_email}')"
+        return f"User('{self.username}', '{self.email}')"
+
 
 class Restaurant(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     rest_name = db.Column(db.String(20), unique=False, nullable=False)
-    tel_number = db.Column(db.Integer, unique=True, nullable=False)
-    food_id = db.Column(db.Integer, db.ForeignKey('food.id'))
-    admin = db.relationship('Admin', backref='restaurant', lazy='dynamic')
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    dishes = db.relationship('Food', secondary=meals, backref=db.backref('meals', lazy='dynamic'))
+    
     def __repr__(self):
-        return f"User('{self.rest_name}', '{self.tel_number}')"
+        return f"User('{self.rest_name}')"
 
 class Food(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     food_name = db.Column(db.String(20), unique=False, nullable=False)
-    price = db.Column(db.Integer, unique=True, nullable=False)
-    restaurant = db.relationship('Restaurant', backref='food', lazy='dynamic')
-
-    def __repr__(self):
-        return f"User('{self.food_name}', '{self.price}')"
-
-class Orders(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}')"
-
-class Role(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    role_name = db.Column(db.String(20), unique=False, nullable=False)
-  
-    def __repr__(self):
-        return f"User('{self.role_name}')"
-
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
-    user = db.relationship('User', backref='admin', lazy='dynamic')
-    role = db.relationship('Role', backref='admin', lazy='dynamic')
+    price = db.Column(db.String(20), unique=False, nullable=False)
+    food = db.Column(db.Integer, db.ForeignKey('user.id'))
     
     def __repr__(self):
-        return f"User('{self.name}', '{self.email}')"
+        return f"User('{self.food_name}', '{self.price}')"
